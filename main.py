d21831df89b8174d104468328ba92a628c21d930
Carlovila <carlos.gvferreira@gmail.com>
2022-11-25 14:34:27 +0000

    One function for both players + error handling

--- main.py
+++ main.py
@@ -1,77 +1,18 @@
 class ticktacktoe():
     def __init__(self):
-        """define board"""
         self.board = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
     def gameboard(self):
         """display the board"""
         print(f"|{self.board[1]}| |{self.board[2]}| |{self.board[3]}|",
               f"|{self.board[4]}| |{self.board[5]}| |{self.board[6]}|",
               f"|{self.board[7]}| |{self.board[8]}| |{self.board[9]}|", sep="\n")
-    def start_game(self, start=1):
-        """start the game (1 starts, 0 stops, set default to 1)"""
+    def startgame(self, start=1):
         if start == 1:
-            print("Tick Tack Toe!")
+            print("Tick-tack-toe!")
             self.gameboard()
-            """x moves first"""
-            return self.move_X()
+            return self.move("X")
         else:
-            """stop if start == 0"""
             return
-    def move_X(self):
-        """checks if the board is full/it's a tie"""
-        if self.tie_check() == False:
-            cell = int(input("Player X, choose number 1-9: "))
-            if cell not in range(1,10):
-                print("Not a valid move")
-                return self.move_X()
-            player = "X"
-            """check if cell is empty before moving"""
-            if self.rule_1(cell) == True:
-                """writes move"""
-                self.board[cell] = "X"
-                self.gameboard()
-                """checks win condition"""
-                if self.win_cond(player) == True:
-                    print("Player X wins!")
-                    """if yes reset the game"""
-                    return self.reset()
-                else:
-                    """if not move to next player"""
-                    return self.move_O()
-            else:
-                """if cell is not empty"""
-                print("Not allowed")
-                return self.move_O()
-        else:
-            print("It's a tie!")
-            return self.reset()
-    def move_O(self):
-        if self.tie_check() == False:
-            cell = int(input("Player O, choose number 1-9: "))
-            if cell not in range(1,10):
-                print("Not a valid move")
-                return self.move_O()
-            player = "O"
-            if self.rule_1(cell) == True:
-                self.board[cell] = "O"
-                self.gameboard()
-                if self.win_cond(player) == True:
-                    print("Player O wins!")
-                    return self.reset()
-                else:
-                    return self.move_X()
-            else:
-                print("Not allowed")
-                return self.move_O()
-        else:
-            print("It's a tie!")
-            return self.reset()
-    def rule_1(self, cell):
-        """checks if cell is empty"""
-        if self.board[cell] == " ":
-            return True
-        else:
-            return False
     def tie_check(self):
         """check for tie"""
         tiecheck = self.board[1:]
@@ -79,6 +20,12 @@ class ticktacktoe():
             return False
         else:
             return True
+    def rule_1(self, cell):
+        """checks if cell is empty"""
+        if self.board[cell] == " ":
+            return True
+        else:
+            return False
     def win_cond(self, player):
         """define win conditions"""
         if self.board[1] == player and self.board[2] == player and self.board[3] == player:
@@ -104,10 +51,37 @@ class ticktacktoe():
         reset = input("Play again? (Y/N): ").upper()
         if reset == "Y":
             self.board = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
-            return self.start_game()
+            return self.startgame()
         elif reset == "N":
-            return self.start_game(0)
+            return self.startgame(0)
         else:
             return self.reset()
-
-ticktacktoe().start_game()
+    def move(self, player):
+        self.player = player
+        if self.tie_check() == False:
+            try:
+                cell = int(input(f"Player {player}, choose number 1-9: "))
+            except ValueError:
+                print("Not valid")
+                return self.move(player)
+            if cell not in range(1,10):
+                print("Not a valid move")
+                return self.move(player)
+            if self.rule_1(cell) == True:
+                self.board[cell] = player
+                self.gameboard()
+                if self.win_cond(player) == True:
+                    print(f"Player {player} wins!")
+                    return self.reset()
+                else:
+                    if player == "X":
+                        return self.move("O")
+                    else:
+                        return self.move("X")
+            else:
+                print("Not allowed")
+                return self.move(player)
+        else:
+            print("It's a tie!")
+            return self.reset()
+ticktacktoe().startgame()
